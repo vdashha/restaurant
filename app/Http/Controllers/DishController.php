81ed0372c46f9ed10847bc206655f00fd7ffbc9e
vdashha <darya.veras8@gmail.com>
2024-11-27 23:36:48 +0300

    add crud for dishes

---
+++ app/Http/Controllers/DishController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Dish;
+use Carbon\Carbon;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class DishController extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+
+
+    public function showDishes(int $category_id)
+    {
+        $dishes = Dish::all();
+        $title = Category::find($category_id)->title;
+        return view('menu.dishes', compact('dishes', 'title'));
+    }
+}
--- resources/views/menu/dishes.blade.php
+++ resources/views/menu/dishes.blade.php
@@ -1,24 +1,31 @@
 @extends('welcome')
 
 @section('content')
-
     <div class="container my-5">
-        <h2 class="text-center">Наши блюда</h2>
+        <h2 class="text-center">{{ $title }}</h2>
         <div class="row justify-content-center">
             @foreach($dishes as $dish)
                 <div class="col-md-4 mb-4">
                     <div class="card h-100">
-                        <img src="{{ asset($dish->image->getUrl()) }}" class="card-img-top" alt="{{ $dish->title }}" style="height: 200px; object-fit: cover;">
-                        <div class="card-body d-flex flex-column">
+                        <img  class="card-img-top" alt="{{ $dish->title }}"
+                             style="height: 200px; object-fit: cover;">
+                        <div class="card-body text-center">
                             <h5 class="card-title">{{ $dish->title }}</h5>
-                            <p class="card-text">{{ $dish->description }}</p> <!-- Добавлено описание блюда -->
-                            <div class="mt-auto">
-                                <a href="{{ route('dish.show', $dish->id) }}" class="btn btn-primary">Подробнее</a> <!-- Ссылка на подробности блюда -->
-                            </div>
+                            <p class="card-text">{{ $dish->description }}</p>
+                            <p class="card-text"><strong>Цена: {{ number_format($dish->price, 2) }} ₽</strong></p>
+                            <p class="card-text"><small>Вес: {{ $dish->weight }} г</small></p>
                         </div>
                     </div>
                 </div>
             @endforeach
         </div>
     </div>
+@endsection
+
+@section('styles')
+    <style>
+        .card {
+            overflow: hidden; /* Скрывает лишние части, если необходимо */
+        }
+    </style>
 @endsection
