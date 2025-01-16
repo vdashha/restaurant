7c739bcac72fa2af1b79f198ce3384346b37f48d
vdashha <darya.veras8@gmail.com>
2025-01-16 15:10:10 +0300

    add ChangeOrderStatus email

---
+++ app/Observers/OrderObserver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Observers;
+
+use App\Events\ChangeOrderStatus;
+use App\Mail\CreateOrder;
+use App\Models\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
+
+class OrderObserver
+{
+    /**
+     * Handle the Order "created" event.
+     */
+    public function created(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "updated" event.
+     */
+    public function updated(Order $order): void
+    {
+        if ($order->isDirty('status'))
+        {
+            event(new ChangeOrderStatus($order));
+        }
+    }
+
+    /**
+     * Handle the Order "deleted" event.
+     */
+    public function deleted(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "restored" event.
+     */
+    public function restored(Order $order): void
+    {
+        //
+    }
+
+    /**
+     * Handle the Order "force deleted" event.
+     */
+    public function forceDeleted(Order $order): void
+    {
+        //
+    }
+}
